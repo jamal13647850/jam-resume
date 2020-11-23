3eee097548a26694162c1c1577b1e0ba2042ca93
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2020-11-23 18:54:57 +0330

    Create app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Switch , Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Layout from "./hoc/Layout/Layout";
+import Intro from "./containers/Intro/Intro";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import AboutMe from "./containers/AboutMe/AboutMe";
+import Contact from "./containers/Contact/Contact";
+
+//fortawesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome, faTh, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faTh, faUser, faEnvelope);
+
+class App extends Component {
+  render() {
+    return (
+             <Layout>
+               <Switch>
+                 <Route exact path="/" component={Intro}/>
+                 <Route path="/portfolio" component={Portfolio}/>
+                 <Route path="/about" component={AboutMe}/>
+                 <Route path="/contact" component={Contact}/>
+               </Switch>
+             </Layout>
+
+    );
+  }
 }
 
 export default App;
